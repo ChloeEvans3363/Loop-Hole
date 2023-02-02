b994e2136d6e89fce440b7676e746eea4ff0293a
LackOfBeef <egan@frelich.us>
2023-02-02 14:46:21 -0500

    Added a script to keep track of overall game variables

---
+++ Loop-Hole/Assets/Scripts/GameScoreManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameScoreManager : MonoBehaviour
+{
+    private float timeElapsed;
+    [SerializeField] private float initialFallSpeed;
+    [SerializeField] private float fallIncreaseRate;
+    [SerializeField] private float fallSpeedCap;
+    private float fallSpeed;
+    private int coins;
+    private int health;
+
+    public float FallSpeed
+    {
+        get { return fallSpeed; }
+        set { fallSpeed = value; }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 3;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        Debug.Log("Time: " + timeElapsed);
+    }
+}
