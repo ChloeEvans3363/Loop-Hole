c8f52cc6a343b13d083d20ce4c8a23607a4200ae
Chloe <70345679+ChloeEvans3363@users.noreply.github.com>
2023-02-17 11:50:46 -0500

    Started trying to remove player input delay

--- Loop-Hole/Assets/Scripts/Player.cs
+++ Loop-Hole/Assets/Scripts/Player.cs
@@ -10,8 +10,10 @@ public class Player : MonoBehaviour
     public float moveSpeed = 0.1f;
     Rigidbody2D rb;
     Vector2 position = new Vector2(0f, 0f);
+    Vector2 test = new Vector2(0f, 0f);
 
     // Start is called before the first frame update
+
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
@@ -20,8 +22,17 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+        mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
         position = Vector2.Lerp(transform.position, new Vector2(mousePosition.x, 0), moveSpeed);
+        //mousePosition.x = Mathf.Clamp(mousePosition.x, Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)).x + transform.GetComponent<SpriteRenderer>().bounds.size.x / 2, Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)).x * -1 - transform.GetComponent<SpriteRenderer>().bounds.size.x / 2);
+        test = new Vector2(mousePosition.x ,0);
+        //Debug.Log(Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z)).x);
+        //Debug.Log(mousePosition.x);
+        //rb.MovePosition(test);
+        //rb.AddForce(new Vector2(mousePosition.x, 0).normalized * 1);
+        //rb.position = mousePosition;
+        //transform.position = mousePosition;
+        //Debug.Log(Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue()).x);
     }
 
     private void FixedUpdate()
