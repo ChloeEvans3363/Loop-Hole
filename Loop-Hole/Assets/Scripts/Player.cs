5a8da8a8bc68e2ed3b5f2f9af0d2b35739534bf4
Chloe <70345679+ChloeEvans3363@users.noreply.github.com>
2023-02-20 10:10:05 -0500

    Player freezes on death

--- Loop-Hole/Assets/Scripts/Player.cs
+++ Loop-Hole/Assets/Scripts/Player.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-
+    public static GameManager gameManager;
     Vector3 mousePosition;
     public float moveSpeed = 0.1f;
     Vector2 position = new Vector2(0f, 0f);
@@ -14,16 +14,22 @@ public class Player : MonoBehaviour
 
     void Start()
     {
-
+        if (gameManager == null)
+        {
+            gameManager = FindObjectOfType<GameManager>();
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
-        position = Vector2.Lerp(transform.position, new Vector2(mousePosition.x, transform.position.y), moveSpeed);
+        if (!gameManager.GetComponent<GameManager>().dead)
+        {
+            mousePosition = Camera.main.ScreenToWorldPoint(Mouse.current.position.ReadValue());
+            position = Vector2.Lerp(transform.position, new Vector2(mousePosition.x, transform.position.y), moveSpeed);
 
-        transform.position = position;
+            transform.position = position;
+        }
 
     }
 }
