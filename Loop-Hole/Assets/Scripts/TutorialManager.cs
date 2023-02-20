48aea8f99fc542729329e683d6da1a1950caf2d1
Chloe <70345679+ChloeEvans3363@users.noreply.github.com>
2023-02-20 11:22:03 -0500

    Worked on tutorial

---
+++ Loop-Hole/Assets/Scripts/TutorialManager.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialManager : MonoBehaviour
+{
+    public static GameManager gameManager;
+    public List<GameObject> listOfObstacles;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (gameManager == null)
+        {
+            gameManager = FindObjectOfType<GameManager>();
+        }
+
+        ResetPosition();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 topBorder = ScreenToWorld(0, Screen.height);
+        transform.Translate(Vector3.up * Time.deltaTime * gameManager.GetComponent<GameManager>().fallSpeed);
+
+        if (transform.position.y > topBorder.y)
+        {
+            //ChangeObsLayout(gameManager.GetComponent<GameManager>().stage - 1);
+            ChangeObsLayout(1);
+        }
+
+    }
+
+    private Vector3 ScreenToWorld(float x, float y)
+    {
+        return Camera.main.ScreenToWorldPoint(new Vector3(x, y, 10));
+    }
+
+
+    private void ResetPosition()
+    {
+        float newYPos = Random.Range(Camera.main.ScreenToWorldPoint(new Vector2(0, -Screen.height)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, -Screen.height * 2)).y);
+        transform.position = new Vector3(transform.position.x, newYPos, 0);
+    }
+
+
+    private void ChangeObsLayout(int stage)
+    {
+        GameObject nextObs = listOfObstacles[stage];
+
+        Instantiate(nextObs);
+        Destroy(this.gameObject);
+    }
+}
