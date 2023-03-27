37ed656b5d181435b708e03b1429538239f2ef6c
Egan Frelich <ejf6912@rit.edu>
2023-03-27 11:34:49 -0400

    Added a wall manager script

---
+++ Loop-Hole/Assets/WallManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WallManager : MonoBehaviour
+{
+    [SerializeField] private List<GameObject> leftWalls;
+    [SerializeField] private List<GameObject> rightWalls;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
