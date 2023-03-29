fe4c3640add9cfba86d12ceb1012905f21c7a40e
Chloe <70345679+ChloeEvans3363@users.noreply.github.com>
2023-03-29 11:12:14 -0400

    Fixed player breaking out of bounds

--- Loop-Hole/Assets/Scripts/Boundaries.cs
+++ Loop-Hole/Assets/Scripts/Boundaries.cs
@@ -4,24 +4,27 @@ using UnityEngine;
 
 public class Boundaries : MonoBehaviour
 {
-    private Vector2 screenBounds;
+    //private Vector2 screenBounds;
     private float playerWidth;
     public GameObject wall;
     private float wallWidth;
+    private float wallX;
 
     // Start is called before the first frame update
     void Start()
     {
-        screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        //screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
         playerWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
         //wallWidth = wall.transform.GetComponent<SpriteRenderer>().bounds.size.x;
-        wallWidth = 1.125f;
+        //wallWidth = 1.125f;
+        wallWidth = transform.localScale.x;
+        wallX = wall.transform.position.x;
     }
 
     private void LateUpdate()
     {
         Vector3 viewPos = transform.position;
-        viewPos.x = Mathf.Clamp(viewPos.x, screenBounds.x * -1 + playerWidth + wallWidth, screenBounds.x - playerWidth - wallWidth);
+        viewPos.x = Mathf.Clamp(viewPos.x, wallX + playerWidth + wallWidth, wallX * -1 - playerWidth - wallWidth);
         transform.position = viewPos;
     }
 }
