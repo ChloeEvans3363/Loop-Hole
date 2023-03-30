e8107bb9a3f6f2d3d2fca5f264359b960ca00cf4
LackOfBeef <egan@frelich.us>
2023-03-30 13:04:58 -0400

    Fixed the wall desync

--- Loop-Hole/Assets/WallManager.cs
+++ Loop-Hole/Assets/WallManager.cs
@@ -6,16 +6,24 @@ public class WallManager : MonoBehaviour
 {
     [SerializeField] private List<GameObject> leftWalls;
     [SerializeField] private List<GameObject> rightWalls;
+    private int[,] wallOffsets; //I should be using math for this instead but I just could not figure it out
     public GameManager gameManager;
     private int topWall = 0;
     private float wallHeight;
+    [SerializeField] private float seamOverlap;
 
     // Start is called before the first frame update
     void Start()
     {
-        if(leftWalls.Count > 0 && rightWalls.Count > 0)
+        if (leftWalls.Count > 0 && rightWalls.Count > 0)
         {
-            wallHeight = leftWalls[0].GetComponent<SpriteRenderer>().bounds.size.y;
+            wallHeight = leftWalls[0].GetComponent<SpriteRenderer>().bounds.size.y - seamOverlap;
+            wallOffsets = 
+                new int[,] { {0, 4, 3, 2, 1}, 
+                             {1, 0, 4, 3, 2}, 
+                             {2, 1, 0, 4, 3}, 
+                             {3, 2, 1, 0, 4}, 
+                             {4, 3, 2, 1, 0} };
         }
     }
 
@@ -32,8 +40,11 @@ public class WallManager : MonoBehaviour
         {
             if(i != topWall)
             {
-                //Trying to figure out the math for this
-                wallList[i].transform.position = new Vector2(wallList[i].transform.position.x, wallList[topWall].transform.position.y - wallHeight * i); 
+                //I should absolutely be using math for this instead
+                //it should always be 0 if i = topWall
+                int wallOffsetMultiplier = wallOffsets[i, topWall];
+                Debug.Log(i + ", " + wallList.Count + ", " + topWall + " = " + wallOffsetMultiplier);
+                wallList[i].transform.position = new Vector2(wallList[i].transform.position.x, wallList[topWall].transform.position.y - wallHeight * wallOffsetMultiplier); 
             }
             else
             {
@@ -43,7 +54,11 @@ public class WallManager : MonoBehaviour
         //Reset wall position
         if (wallList[topWall].transform.position.y > wallHeight)
         {
-            topWall = (topWall+1)%wallList.Count;
+            topWall++;
+            if(topWall >= wallList.Count)
+            {
+                topWall = 0;
+            }
         }
     }
 }
