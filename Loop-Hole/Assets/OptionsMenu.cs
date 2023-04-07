60a69086654194a1c553452fea154c71315f2f1c
Egan Frelich <ejf6912@rit.edu>
2023-04-07 11:27:33 -0400

    Wrote functions for the options menu

--- Loop-Hole/Assets/OptionsMenu.cs
+++ Loop-Hole/Assets/OptionsMenu.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class OptionsMenu : MonoBehaviour
 {
+    [SerializeField] private GameObject mainPanel;
+    [SerializeField] private GameObject optionsPanel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,26 +21,40 @@ public class OptionsMenu : MonoBehaviour
 
     public void SetMasterVolume(float value)
     {
-
+        PlayerPrefs.SetFloat("masterVolume", value);
     }
 
     public void SetSFXVolume(float value)
     {
-
+        PlayerPrefs.SetFloat("sfxVolume", value);
     }
 
     public void SetMusicVolume(float value)
     {
-
+        PlayerPrefs.SetFloat("musicVolume", value);
     }
 
     public void ReturnToMain()
     {
+        mainPanel.SetActive(true);
+        optionsPanel.SetActive(false);
+        PlayerPrefs.Save();
+    }
 
+    public void GoToOptions()
+    {
+        mainPanel.SetActive(false);
+        optionsPanel.SetActive(true);
     }
 
     public void ToggleScreenshake(bool shake)
     {
-
+        if(shake)
+        {
+            PlayerPrefs.SetInt("screenshakeEnabled", 1);
+        } else
+        {
+            PlayerPrefs.SetInt("screenshakeEnabled", 0);
+        }
     }
 }
