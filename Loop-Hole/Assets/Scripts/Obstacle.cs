c7215900e9d7ad92c4760839df66fbb7baa8d307
Chloe <cre3363@g.rit.edu>
2023-04-10 18:32:23 -0400

    Created moving platforms

--- Loop-Hole/Assets/Scripts/Obstacle.cs
+++ Loop-Hole/Assets/Scripts/Obstacle.cs
@@ -6,12 +6,25 @@ public class Obstacle : MonoBehaviour
 {
     public static GameManager gameManager;
     [SerializeField] private bool move;
+    [SerializeField] GameObject wall;
+    private float obstacleWidth;
+    private float wallWidth;
+    private float wallX;
+    private Vector2 limit;
+    private float speed = 2;
 
     void Start()
     {
         if (gameManager == null)
         {
             gameManager = FindObjectOfType<GameManager>();
+        }
+        if (move)
+        {
+            obstacleWidth = transform.GetComponent<SpriteRenderer>().bounds.size.x / 2;
+            wallWidth = wall.transform.localScale.x;
+            wallX = wall.transform.position.x;
+            limit = new Vector2(wallX  + wallWidth + obstacleWidth, transform.position.y);
         }
     }
 
@@ -33,6 +46,10 @@ public class Obstacle : MonoBehaviour
 
     private void Move()
     {
-
+        if(Vector2.Distance(transform.position, limit) < 0.02f)
+        {
+            limit = new Vector2(limit.x*-1, limit.y);
+        }
+        transform.position = Vector2.MoveTowards(transform.position, limit, speed * Time.deltaTime);
     }
 }
