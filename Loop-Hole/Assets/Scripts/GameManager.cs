49e7268ea1cd9072292c14f17681b49ae8e630c6
Chloe <70345679+ChloeEvans3363@users.noreply.github.com>
2023-04-12 11:38:41 -0400

    Added in obstacle sprite to all the prefabs

--- Loop-Hole/Assets/Scripts/GameManager.cs
+++ Loop-Hole/Assets/Scripts/GameManager.cs
@@ -206,7 +206,7 @@ public class GameManager : MonoBehaviour, IDataPersistence
     /// <param name="amt"></param>
     public void TakeDamage(int amt)
     {
-        /*
+        
         if(iTime <= 0f && health != 0 && !tutorial)
         {
             iTime = 1.5f;
@@ -224,7 +224,6 @@ public class GameManager : MonoBehaviour, IDataPersistence
             audioManager.Play("Pain");
             OnDamage();
         }
-        */
         
     }
 
